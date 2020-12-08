f3c8f139b677e18c2fb82138b71f0567e995811c
BJWiley233 <bwiley4@jh.edu>
2020-12-08 00:50:40 -0500

    update mlpack script

--- file2.cpp
+++ file2.cpp
@@ -3,3 +3,33 @@
 #include <armadillo>//ml pack is dependent on armadillo
 
 Using namespace std;
+
+int main() {
+
+    int k = 2; //amount of clusters
+    int dim = 2;//dimensionality of program
+    int samples = 50; 
+    int max_iter = 10;//maximum number of iterations
+
+    arma::mat data(dim, samples, arma::fill::zeros);
+
+    // create data
+    int i = 0;
+    for(; i < samples / 2; ++i) {
+        data.col(i) = arma::vec({1, 1}) + 0.25*arma::randn<arma::vec>(dim);
+    }
+    for(; i < samples; ++i) {
+        data.col(i) = arma::vec({2, 3}) + 0.25*arma::randn<arma::vec>(dim);
+    }
+
+    //cluster the data
+    arma::Row<size_t> clusters;
+    arma::mat centroids;
+
+    mlpack::kmeans::KMeans<> mlpack_kmeans(max_iter);
+    mlpack_kmeans.Cluster(data, k, clusters, centroids);
+
+    centroids.print("Centroids:");
+
+
+}
